f3cdf0ea91c7b8b88edee0f5b8ed54322e6de06f
chefTee <taiwosharon36@gmail.com>
2025-06-03 03:17:55 +0100

    update

--- template2form.js
+++ template2form.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }else{
             alert('PDF generation is only allowed on desktop for the best experience.');
             downloadButton.style.display = 'none';
-            location.reload();
+            history.back();
     }
     //Getting the preview of the image
     imageInput.addEventListener('change', (e) => {
